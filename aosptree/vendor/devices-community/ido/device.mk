063f78ebc1ad2ea3181728c386b0b1da356c36c4
nojick <nojick@gmail.com>
2024-09-20 19:52:57 +0300

    ido: disable codecs glodroid
    codecs need to be updated

--- aosptree/vendor/devices-community/ido/device.mk
+++ aosptree/vendor/devices-community/ido/device.mk
@@ -6,6 +6,7 @@ $(call inherit-product, glodroid/configuration/common/device-common.mk)
 
 GD_NO_DEFAULT_GRAPHICS := true
 GD_USE_RS_HWCOMPOSER := true
+GD_NO_DEFAULT_CODECS := true
 GD_NO_DEFAULT_FASTBOOTD := true
 GD_NO_DEFAULT_BOOTCTL   := true
 GD_NO_DEFAULT_CAMERA    := true
