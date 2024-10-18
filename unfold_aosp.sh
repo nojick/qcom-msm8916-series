e73ce5cfe5d03dfceb81498530d30227d90aaea9
nojick <nojick@gmail.com>
2024-10-18 22:02:12 +0300

    manifest-aosp: update vesion android to android-15.0.0_r3

--- unfold_aosp.sh
+++ unfold_aosp.sh
@@ -4,7 +4,7 @@ LOCAL_PATH=$(pwd)
 
 echo Init repo tree using AOSP manifest
 pushd aosptree
-repo init -u https://android.googlesource.com/platform/manifest -b refs/heads/android-15.0.0_r1
+repo init -u https://android.googlesource.com/platform/manifest -b refs/heads/android-15.0.0_r3
 cd .repo/manifests
 mv default.xml aosp.xml
 cp ${LOCAL_PATH}/manifests/glodroid.xml glodroid.xml
